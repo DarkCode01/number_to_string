dac42ef1c36a8c41ab5df66ef45300224486da5e
Jose M. Segura Polanco <j0s3s3gur4@gmail.com>
2018-01-15 19:58:25 -0900

    Add files via upload

--- script.py
+++ script.py
@@ -1,40 +1,20 @@
 #! /usr/bin/env python3
-"""
-    This is a module capable of returning the amount of a number
-    literally.
-"""
+
 import sys
 from termcolor import colored
 from numeros_ import LISTA_NUMEROS, NUMEROS_RAROS
 
 
 class NumberToString(object):
-    """
-        This is a module capable of returning the amount of a number
-        literally.
 
-        This module is for educational purposes, is developed based on
-        recursion looking for a simple and friendly way for people who
-        are beginning to learn python.
-
-        example:
-        >>> input ("1,000,000")
-        >>> output: "(uno|un) millon(es)"
-
-        Note: The number entered must be separated with commas.
-    """
-
-    def __init__(self, number=None):
-        if number:
-            self.result = ""
-            self.number_copy = number
-            self.number = number.split(",")
-            self.len_ = len(self.number)
-            self.number = [n.replace(" ", "") for n in self.number]
-            self.LN = LISTA_NUMEROS
-            self.vueltas = len(self.number)
-            self.len_group = ""
-        pass
+    def __init__(self, number):
+        self.result = ""
+        self.number_copy = number
+        self.number = number.split(",")
+        self.len_ = len(self.number)
+        self.number = [n.replace(" ", "") for n in self.number]
+        self.LN = LISTA_NUMEROS
+        self.len_group = ""
 
     def help(self, e=None):
         if e:
@@ -54,25 +34,13 @@ class NumberToString(object):
             return (colored("", "white"), e)
         else:
             with open("man.txt", "r") as fl:
-                return (colored("Help", "white"), colored(fl.readlines(), "green"))
+                return (colored("Help", "white"), colored(fl.read(), "green"))
 
     def find(self):
-        """
-            This metidi will take care of returning a tuple that will
-            contain the number that had been entered and the amount literally.
-
-            Example:
-                (1,000, "(uno|un) mil") -> Tuple returned
-                    ^            ^
-                    |            |
-                self.number | self.result
-                |\
-                (Number entered)
-        """
 
         try:
             if len(self.number) == 0:
-                return self.result
+                return 0
 
             self.len_group = str(len(self.number[0]))
             for i, num in enumerate(self.number[0]):
@@ -84,12 +52,11 @@ class NumberToString(object):
                     self.result += " "
                     break
                 else:
-                    if int(num) == 1 and self.len_ == 2:
-                        pass
-                    elif int(num) >= 1:
+                    if int(num) >= 1:
                         self.result += self.LN[self.len_group][int(num) - 1]
                         self.result += " "
-                        self.result += "y " if int(self.len_group) == 2 else ""
+                        if self.len_group == "2" and self.number[0][i+1] != "0":
+                            self.result += "y " 
 
                 self.len_group = int(self.len_group) - 1
                 self.len_group = str(self.len_group)
@@ -99,7 +66,6 @@ class NumberToString(object):
                 self.result += ", "
 
             self.len_ -= 1
-            self.vueltas -= 1
             self.number.pop(0)
             self.find()
             return (colored(self.number_copy, "red"),
@@ -110,11 +76,11 @@ class NumberToString(object):
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "--help":
-        new = NumberToString()
-        title, text = new.help()
-        print(title, ": \n\t", text)
-    elif sys.argv[1]:
+    try:
         new = NumberToString(sys.argv[1])
         num, string = new.find()
         print(num, " -> ", string)
+    except Exception as e:
+        print(colored("\t script 1215, Falto el numero", "red"))
+        print(e)
+
